3f8b9fc87c9e5fd792ea86696279029d8b25784a
Peter <pgnokes@gmail.com>
2020-12-05 21:02:42 +0000

    Age now changes automatically
    Removed meta for Twitter as it didn't work

---
+++ index.js
@@ -0,0 +1,11 @@
+var dateString = "2002-06-21"; //Date of Birth
+
+
+function calcAge(dateString) {
+  var birthday = +new Date(dateString);
+  return ~~((Date.now() - birthday) / (31557600000)); // 31557600000 is 24 * 3600 * 365.25 * 1000
+}
+
+function startUp(){
+    document.getElementById("desc").innerHTML = calcAge(dateString) + " Year old Programmer from London, United Kingdom"
+}
