ebdfe1ee5a30588e786c433dfa5450fee08a68ed
Peter <peter@peternokes.co.uk>
2023-09-16 20:58:52 +0100

    Redesigned Website

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,17 @@
-function betamessage(){
-    document.getElementById("betamessage").classList.add("goodbye")
-}
-
-var dateString = "2002-06-21"; //Date of Birth
-
-calcAge(dateString);
+var dateString = "21/06/2002"; // Date of Birth
 
 function calcAge(dateString) {
-  var birthday = +new Date(dateString);
+  var birthday = +new Date(dateString.split('/').reverse().join('/'));
   return ~~((Date.now() - birthday) / (31557600000)); // 31557600000 is 24 * 3600 * 365.25 * 1000
+}
+
+document.getElementById("age").innerText = calcAge(dateString);
+
+function enableNavBar() {
+  var x = document.getElementById("navbar");
+  if (x.classList.contains("responsive")) {
+    x.classList.remove("responsive");
+  } else {
+    x.classList.add("responsive");
+  }
 }
